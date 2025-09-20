b53d577a11005406f16e6356a1fd4e036b1d7af6
Rahul Vishwakarma <165129391+rahulVishwakarma03@users.noreply.github.com>
2025-09-20 12:46:57 +0530

    Add all test cases and final output statement

--- lion-hunt/lion_hunt.js
+++ lion-hunt/lion_hunt.js
@@ -1,3 +1,17 @@
 // ### Lion Hunt..
 
-console.log("Lion Hunt");
+const testCase1 = "LZ";
+const testCase2 = "Z L";
+const testCase3 = "L     Z";
+const testCase4 = "L     L";
+const testCase5 = "Z   Z   Z";
+const testCase6 = "L  ZL Z";
+const testCase7 = "L ZL";
+const testCase8 = " L ZL";
+
+const testCaseToUse = testCase1;
+
+// Write your code here:
+
+
+console.log("Input: ", testCaseToUse, "\nOutput: ", /* your result here */);
