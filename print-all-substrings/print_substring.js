536840932d635069868e4c22295e74550859dbf6
Rahul Vishwakarma <165129391+rahulVishwakarma03@users.noreply.github.com>
2025-09-21 16:39:15 +0530

    Remove outer loop and add variable and else-if block
    Add else-if block to assign new starting index for different substring and reset currIndex, substr and endIndex.

--- print-all-substrings/print_substring.js
+++ print-all-substrings/print_substring.js
@@ -1,13 +1,17 @@
 const str = "abcd";
 
-for(let startIndex=0; startIndex<str.length; startIndex++){
-    let substr = "";
-    let endIndex = startIndex;
-    for(let currIndex = startIndex; currIndex <= endIndex; currIndex++){
-        substr = substr + str[currIndex];
-        if(currIndex === endIndex && endIndex < str.length){
-            console.log(substr);
-            endIndex++;
-        }
-    }
+let startIndex = 0;
+let substr = "";
+let endIndex = startIndex;
+for (let currIndex = startIndex; currIndex <= endIndex; currIndex++) {
+  substr = substr + str[currIndex];
+  if (currIndex === endIndex && endIndex < str.length) {
+    console.log(substr);
+    endIndex++;
+  } else if (endIndex === str.length && startIndex < str.length) {
+    startIndex = startIndex + 1;
+    currIndex = startIndex - 1;
+    substr = "";
+    endIndex = startIndex;
+  }
 }
