0da91d0a7600466ed183e9ac6e432820b4593c71
Rahul Vishwakarma <165129391+rahulVishwakarma03@users.noreply.github.com>
2025-09-21 15:08:11 +0530

    Create a variable named substr and perform concatenation operation in inner most loop to store all characters of one substring in substr variable

--- print-all-substrings/print_substring.js
+++ print-all-substrings/print_substring.js
@@ -1,9 +1,11 @@
-const str = "abc";
+const str = "abcd";
 
 for(let startIndex=0; startIndex<str.length; startIndex++){
     for(let endIndex = startIndex; endIndex<str.length; endIndex++){
+        let substr = "";
         for(let currIndex = startIndex; currIndex<=endIndex; currIndex++){
-            console.log(str[currIndex]);
+            substr = substr + str[currIndex];
         }
+        console.log(substr);
     }
 }
