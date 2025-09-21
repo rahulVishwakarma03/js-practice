4627855871bff3281eb70611e074af4c0283a687
Rahul Vishwakarma <165129391+rahulVishwakarma03@users.noreply.github.com>
2025-09-21 15:04:20 +0530

    Add one more loop inside inner loop to print all character of the substrings.

--- print-all-substrings/print_substring.js
+++ print-all-substrings/print_substring.js
@@ -2,8 +2,8 @@ const str = "abc";
 
 for(let startIndex=0; startIndex<str.length; startIndex++){
     for(let endIndex = startIndex; endIndex<str.length; endIndex++){
-
-        console.log(startIndex, endIndex);
-       
+        for(let currIndex = startIndex; currIndex<=endIndex; currIndex++){
+            console.log(str[currIndex]);
+        }
     }
 }
